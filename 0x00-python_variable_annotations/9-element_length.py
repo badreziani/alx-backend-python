40501cce9d06802dfbdbf11b80fb2939edbdc802
Badre Ziani <badreziani@gmail.com>
2024-10-11 15:13:39 +0100

    Adding doc string to task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Retruns a list of tuples"""
     return [(i, len(i)) for i in lst]

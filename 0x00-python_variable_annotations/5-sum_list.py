e4acfc6bb18f643186cbdb1d4e32b1ca681ec52a
Badre Ziani <badreziani@gmail.com>
2024-10-11 15:10:21 +0100

    Fix task 5 errors

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 5-sum_list module
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of floats"""
 
     return sum(input_list)
